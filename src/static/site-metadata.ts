d88f7582e0c8fe6545babe3bb65819cc075a4a4d
dafengy <112263692+dafengy@users.noreply.github.com>
2024-11-06 18:43:18 +0800

    logo url

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: '运动是生命的绽放',
   siteUrl: 'https://dafengy.github.io/',
-  logo: 'https://mmbiz.qpic.cn/mmbiz_png/X9gVKvCVLk095dC5jEdKmYDwjJyNYYhCHz8LQtTcgoDnYOyB4x67lRe69ZHVJbtj24FG1LdkuyRLxdJiaJQsQ2g/640.png',
+  logo: 'https://imgse.com/i/pAszzQO',
   description: '跑啊，大兄弟',
   navLinks: [
     {
